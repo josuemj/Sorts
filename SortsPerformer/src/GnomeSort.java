31a7fed00064bb4109ad6599077edd0e63d1f89b
josuemj <98498772+josuemj@users.noreply.github.com>
2023-02-07 17:45:26 -0600

    Gnome sort

--- SortsPerformer/src/GnomeSort.java
+++ SortsPerformer/src/GnomeSort.java
@@ -1,2 +1,29 @@
+/**
+ * Class contains the Gnome sort
+ * Source: https://www.geeksforgeeks.org/java-program-for-gnome-sort/
+ */
 public class GnomeSort {
+
+    /**
+     * where n is the array size
+     * @param arr
+     * @param n
+     */
+    public static void gnomeSort(int arr[], int n) {
+        int index = 0;
+
+        while (index < n) {
+            if (index == 0)
+                index++;
+            if (arr[index] >= arr[index - 1])
+                index++;
+            else {
+                int temp = 0;
+                temp = arr[index];
+                arr[index] = arr[index - 1];
+                arr[index - 1] = temp;
+                index--;
+            }
+        }
+    }
 }
