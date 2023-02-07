f3ac225d63ac060b35442b2d77b7adea0f8655bd
josuemj <98498772+josuemj@users.noreply.github.com>
2023-02-07 16:35:59 -0600

    Gnome sort implemented

---
+++ A/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int o = 1;
+        while (o>0){
+           System.out.println("Hello world!");
+           o=+1;
+        }
+    }
+}
