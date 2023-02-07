a55db210674e232fc00659775e1e06bbc0df7ff4
josuemj <98498772+josuemj@users.noreply.github.com>
2023-02-07 17:25:55 -0600

    Restarted proyects

--- A/src/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        int o = 1;
-        while (o>0){
-           System.out.println("Hello world!");
-           o=+1;
-        }
-    }
-}
