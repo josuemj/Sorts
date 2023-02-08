242ba1cef2b24a41b81c25af85cd20d411914682
josuemj <98498772+josuemj@users.noreply.github.com>
2023-02-07 18:13:29 -0600

    Radix sort and implementation samples scripted

--- SortsPerformer/src/Main.java
+++ SortsPerformer/src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Implementations here
+
+        //Implementation sample
+
+        //Random array
+        int[] unsortArray = {1,2,4,2,332,23,2}; //Array will be given then.
+
+        // Sort function Call
+        RadixSort.radixsort(unsortArray, unsortArray.length);
+        GnomeSort.gnomeSort(unsortArray,unsortArray.length);
+
+        //                    array               Start                       end
+        QuickSort.quickSort(unsortArray,unsortArray[0],unsortArray[unsortArray.length-1]);
+
+
+
     }
 }
